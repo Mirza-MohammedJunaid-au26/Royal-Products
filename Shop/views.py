a655d13524cf524fd93d5bedfbcf3e460e6b5c6c
Mirza-MohammedJunaid-au26 <spidy8655@gmail.com>
2021-11-26 22:20:42 +0530

    Home Slide

--- Shop/views.py
+++ Shop/views.py
@@ -6,12 +6,19 @@ from math import ceil
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
-    n = len(products)
-    nslides = n//4 + ceil((n/4)+(n//4))
+    # products = Product.objects.all()
 #    params = {"no_of_sldes":nslides , "range" : range(1,nslides) ,"products" : products}
-    allProds = [[products,range(1,nslides),nslides],
-    [products,range(1,nslides),nslides]]
+    # allProds = [[products,range(1,nslides),nslides],
+    # [products,range(1,nslides),nslides]]
+    allProds = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category = cat)
+        n = len(prod)
+        nslides = n//4 + ceil((n/4)+(n//4))
+        allProds.append([prod,range(1,nslides),nslides]) 
+
     params = {"allProds":allProds}
     return render(request,'shop/index.html',params)
 
